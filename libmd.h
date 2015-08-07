d27bea1278188b1330f7789cab09cb78aa0c3069
Bhaskar <bdutta@gmail.com>
2015-08-07 11:12:51 -0500

    cephes files added

---
+++ libmd.h
@@ -0,0 +1,15 @@
+/*
+* The Complete Cephes Math Library Release 2.2: June, 1992
+* Copyright 1984, 1987, 1988, 1992 by Stephen L. Moshier
+* Direct inquiries to 30 Frost Street, Cambridge, MA 02140
+*/
+
+/* basic maths */
+double sqrt(double a);
+
+/* Normal Distribution Function */
+double ndtr(double a);
+
+/* Students T Distribution Function */
+double stdtr(int k, double t );
+double stdtri(int k, double p);
